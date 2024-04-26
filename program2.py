f11050a7f82a7e18d0f1c4c69a196ef39f96517b
sagar chauhan <163311217+sagar1chauhan@users.noreply.github.com>
2024-04-26 16:37:51 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,15 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+def romanToInt(s: str) -> int:
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
+
+    for char in reversed(s):
+        value = roman_values[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total
+
